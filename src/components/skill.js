2e755b9bdce46a30adb71cacda24f0314ce049bc
Supakrit Aphonmaeklong <supakritaphonmaeklong@gmail.com>
2023-12-04 21:20:29 +0700

    react tool tip color should match theme state

--- src/components/skill.js
+++ src/components/skill.js
@@ -5,17 +5,20 @@ import { Card } from "@nextui-org/react";
 import Image from "next/image";
 import { Tooltip } from "react-tippy";
 import "react-tippy/dist/tippy.css";
+import { useTheme } from "@/context/theme-context";
 
 export default function Skill({ skillText, skillUrl }) {
+  const { theme } = useTheme();
   return (
     <Tooltip
+      key={theme}
       title={skillText}
       position="bottom"
       trigger="mouseenter"
       arrow={true}
       duration={300}
       animation="fade"
-      theme="dark"
+      theme={theme === "light" ? "light" : "dark"}
     >
       <Card className="group hover:scale-110 transition duration-300 ease-in-out rounded-full shadow-sm borderBlack">
         <div className="flex shadow-lg justify-center items-center w-[4rem] h-[4rem] sm:w-[6rem] sm:h-[6rem] bg-white dark:bg-white/10">
